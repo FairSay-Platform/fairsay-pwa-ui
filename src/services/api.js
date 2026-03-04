6cf5c6c3414f31c33e6ba396d98b08e4e56f45b5
Cynnin <cynone63@gmail.com>
2026-03-04 15:26:59 +0100

    I swear I fixed this before

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Live Render Backend URL
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 ? import.meta.env.VITE_API_BASE_URL + "/api"
-: "https://fairsay-backend.onrender.com/api";
+: "https://fairsay-api.onrender.com/api";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
